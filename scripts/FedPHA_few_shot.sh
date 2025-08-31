622a2ead13e8068449d4e8297ceacae055b21bc4
ChengYing Fang <fangcy@whu.edu.cn>
2025-09-01 04:52:43 +0800

    Update FedPHA_few_shot.sh

--- scripts/FedPHA_few_shot.sh
+++ scripts/FedPHA_few_shot.sh
@@ -1,3 +1,20 @@
 # bash scripts/FedPHA_few_shot.sh
 # trainers = ['PROMPTFL', 'PROMPTFL_PROX', 'FEDPGP', 'GLP_OT', 'GL_SVDMSE']
-python federated_main.py --trainer GL_SVDMSE --dataset caltech101
+# python federated_main.py --trainer GL_SVDMSE --dataset caltech101
+
+TRAINER="GL_SVDMSE"     # choose from the list above
+DATASET="caltech101"    # dataset name
+SHOTS=2                 # number of shots per class
+BACKBONE="rn50"         # backbone model: rn50 | vit_b16 | ...
+USERS=10                # number of federated clients
+SEED=1                  # random seed
+OUTPUT_DIR="output/${DATASET}/${TRAINER}/shots_${SHOTS}/seed_${SEED}"
+
+python federated_main.py \
+  --trainer ${TRAINER} \
+  --dataset ${DATASET} \
+  --shots ${SHOTS} \
+  --backbone ${BACKBONE} \
+  --num_users ${USERS} \
+  --seed ${SEED} \
+  --output_dir ${OUTPUT_DIR}
